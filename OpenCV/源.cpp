9f860c2688f0c6232717894c244a9edc664371a4
昕宇 王 <2250306810@qq.com>
2018-04-12 11:10:14 +0800

    感动，全部改为Mat形式，没有bug！

--- OpenCV/源.cpp
+++ OpenCV/源.cpp
@@ -1,12 +1,14 @@
-#include <iostream>  
-#include "cv.h"  
-#include "cxcore.h"  
-#include "highgui.h"  
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
+
 using namespace std;
 using namespace cv;
 
 
-CvPoint getNextMinLoc(IplImage *result, CvPoint minLoc, int maxVaule, int templatW, int templatH)
+Point getNextMinLoc(Mat result, Point minLoc, int maxVaule, int templatW, int templatH)
 {
 
 	// 先将第一个最小值点附近两倍模板宽度和高度的都设置为最大值防止产生干扰  
@@ -22,45 +24,46 @@ CvPoint getNextMinLoc(IplImage *result, CvPoint minLoc, int maxVaule, int templa
 	{
 		startY = 0;
 	}
-	if (endX > result->width - 1)
+	if (endX > result.cols - 1)
 	{
-		endX = result->width - 1;
+		endX = result.cols - 1;
 	}
-	if (endY > result->height - 1)
+	if (endY > result.rows - 1)
 	{
-		endY = result->height - 1;
+		endY = result.rows - 1;
 	}
 	int y, x;
 	for (y = startY; y <= endY; y++)
 	{
 		for (x = startX; x <= endX; x++)
 		{
-			cvSetReal2D(result, y, x, maxVaule);
+			result.at<float>(y,x) = maxVaule;
 		}
 	}
 	// 然后得到下一个最小值并且返回  
 	double new_minVaule, new_maxValue;
-	CvPoint new_minLoc, new_maxLoc;
-	cvMinMaxLoc(result, &new_minVaule, &new_maxValue, &new_minLoc, &new_maxLoc);
+	Point new_minLoc, new_maxLoc;
+	minMaxLoc(result, &new_minVaule, &new_maxValue, &new_minLoc, &new_maxLoc, Mat());
 	return new_minLoc;
 
 }
 int main()
 {
-	IplImage *src = cvLoadImage("原图像.png", 0);
-	IplImage *srcResult = cvLoadImage("原图像.png", 3);  //用来显示  
-	IplImage *templat = cvLoadImage("模板.png", 0);
-	IplImage *result;  // 用来存放结果  
-	if (!src || !templat)
+	Mat src = imread("原图像.png", 0);
+	Mat srcResult = imread("原图像.png", 3);  //用来显示  
+	Mat templat = imread("模板.png", 0);
+	Mat result;// 用来存放结果  
+
+	if (src.empty() || templat.empty())
 	{
 		cout << "打开图片失败" << endl;
 		return 0;
 	}
 	int srcW, srcH, templatW, templatH, resultH, resultW;
-	srcW = src->width;
-	srcH = src->height;
-	templatW = templat->width;
-	templatH = templat->height;
+	srcW = src.cols;
+	srcH = src.rows;
+	templatW = templat.cols;
+	templatH = templat.rows;
 	if (srcW < templatW || srcH < templatH)
 	{
 		cout << "模板不能比原图小" << endl;
@@ -68,32 +71,29 @@ int main()
 	}
 	resultW = srcW - templatW + 1;
 	resultH = srcH - templatH + 1;
-	result = cvCreateImage(cvSize(resultW, resultH), 32, 1);    //  匹配方法计算的结果最小值为float  
-	cvMatchTemplate(src, templat, result, CV_TM_SQDIFF);
+	result.create(resultW, resultH, CV_32FC1);    //  匹配方法计算的结果最小值为float  
+	matchTemplate(src, templat, result, CV_TM_SQDIFF);
 	double minValue, maxValue;
-	CvPoint minLoc, maxLoc;
-	cvMinMaxLoc(result, &minValue, &maxValue, &minLoc, &maxLoc);
-	cvRectangle(srcResult, minLoc, cvPoint(minLoc.x + templatW, minLoc.y + templatH), cvScalar(0, 0, 255));
-	CvPoint new_minLoc;
+	Point minLoc, maxLoc;
+	minMaxLoc(result, &minValue, &maxValue, &minLoc, &maxLoc, Mat());
+	rectangle(srcResult, minLoc, cvPoint(minLoc.x + templatW, minLoc.y + templatH), cvScalar(0, 0, 255));
+	Point new_minLoc;
 
 	// 计算下一个最小值  
 	new_minLoc = getNextMinLoc(result, minLoc, maxValue, templatW, templatH);
-	while(cvGetReal2D(result, new_minLoc.y, new_minLoc.x) < 0.5*minValue+0.5*maxValue)
+	while(result.at<float>(new_minLoc.y, new_minLoc.x) < 0.5*minValue+0.5*maxValue)
 	{
 		cout << new_minLoc.y << " , " << new_minLoc.x << endl;
-		cvRectangle(srcResult, new_minLoc, cvPoint(new_minLoc.x + templatW, new_minLoc.y + templatH), cvScalar(0, 0, 255));
+		rectangle(srcResult, new_minLoc, cvPoint(new_minLoc.x + templatW, new_minLoc.y + templatH), cvScalar(0, 0, 255));
 		new_minLoc = getNextMinLoc(result, new_minLoc, maxValue, templatW, templatH);
 	}
 
 	//cvRectangle(srcResult, new_minLoc, cvPoint(new_minLoc.x + templatW, new_minLoc.y + templatH), cvScalar(0, 0, 255));
 	cvNamedWindow("srcResult", 0);
 	cvNamedWindow("template", 0);
-	cvShowImage("srcResult", srcResult);
-	cvShowImage("template", templat);
+	imshow("srcResult", srcResult);
+	imshow("template", templat);
 	cvWaitKey(0);
-	cvReleaseImage(&result);
-	cvReleaseImage(&templat);
-	cvReleaseImage(&srcResult);
-	cvReleaseImage(&src);
+
 	return 0;
 }
