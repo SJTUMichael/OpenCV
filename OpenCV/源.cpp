8f669f9c37ccc9ceaf06a85309592166f9a158e8
昕宇 王 <2250306810@qq.com>
2018-05-16 21:19:23 +0800

    提交一次master以和其他分支分开

--- OpenCV/源.cpp
+++ OpenCV/源.cpp
@@ -78,7 +78,7 @@ bool MyTemplateMatch(Mat src, Mat templat, vector<Point2i> &TargetPoint, Point o
 	minMaxLoc(result, &minValue, &maxValue, &minLoc, &maxLoc, Mat());
 	if (minValue > 0.2) return 0; //没找到
 
-	//TargetPoint.push_back(minLoc+offset);
+								  //TargetPoint.push_back(minLoc+offset);
 	AddPoint(TargetPoint, minLoc + offset, templat.cols, templat.rows);
 
 	Point new_minLoc;
@@ -98,16 +98,17 @@ bool MyTemplateMatch(Mat src, Mat templat, vector<Point2i> &TargetPoint, Point o
 
 
 int main()
-{	Mat src0, srcResult, templat, src, result; // result用来存放结果，src0为原图像，src为扩展边界后图像
+{
+	Mat src0, srcResult, templat, src, result; // result用来存放结果，src0为原图像，src为扩展边界后图像
 	char filename[100];
 	//srcResult = imread("C:\\Users\\Mark\\Desktop\\测试素材\\data1\\0.png", 1);  //用来显示 
 	templat = imread("C:\\Users\\Mark\\Desktop\\测试素材\\data1\\mold\\mold.png", 0);
 
-	for (unsigned int i = 0; i <= 12;++i)
+	for (unsigned int i = 0; i <= 12; ++i)
 	{
 		sprintf(filename, "C:\\Users\\Mark\\Desktop\\测试素材\\data1\\%d.png", i);
 		src = imread(filename, 0);
-		
+
 		if (src.empty() || templat.empty())
 		{
 			cout << "打开图片失败" << endl;
@@ -119,31 +120,31 @@ int main()
 
 		//copyMakeBorder(src0, src, 0, 0, templat.cols, templat.cols, BORDER_CONSTANT, Scalar(0,0,0)); //扩展待匹配的图像，本批图像工件从左向右进入，所以扩展图像左右
 		srcResult = src.clone(); //查看结果的图像
-	
-		
+
+
 		vector<Point2i> TargetPoint;
 		Point minLoc;
 
-		MyTemplateMatch(src, templat, TargetPoint, Point(0,0));
-		
+		MyTemplateMatch(src, templat, TargetPoint, Point(0, 0));
+
 
 		int leftTemW, rightTemW;
 		leftTemW = rightTemW = templat.cols / 2;
 
 		Rect leftTempRect(0, 0, leftTemW, templat.rows - 1);
 		Mat leftTemplat(templat, leftTempRect);
-		Rect rightSrcRect(src.cols-templat.cols-1, 0, templat.cols, src.rows - 1);
+		Rect rightSrcRect(src.cols - templat.cols - 1, 0, templat.cols, src.rows - 1);
 		Mat rightSrc(src, rightSrcRect);
-		MyTemplateMatch(rightSrc, leftTemplat, TargetPoint, Point(src.cols - templat.cols-1, 0));
+		MyTemplateMatch(rightSrc, leftTemplat, TargetPoint, Point(src.cols - templat.cols - 1, 0));
 
-		Rect rightTempRect(templat.cols - rightTemW-1, 0,rightTemW , templat.rows - 1);
+		Rect rightTempRect(templat.cols - rightTemW - 1, 0, rightTemW, templat.rows - 1);
 		Mat rightTemplat(templat, rightTempRect);
-		Rect leftSrcRect(0, 0, templat.cols, src.rows-1);
+		Rect leftSrcRect(0, 0, templat.cols, src.rows - 1);
 		Mat leftSrc(src, leftSrcRect);
-		MyTemplateMatch(leftSrc, rightTemplat, TargetPoint, Point(0-(templat.cols - rightTemW - 1), 0));
-		
+		MyTemplateMatch(leftSrc, rightTemplat, TargetPoint, Point(0 - (templat.cols - rightTemW - 1), 0));
 
-		for(int j = TargetPoint.size()-1; j>=0; --j)//画出目标点对应的矩形
+
+		for (int j = TargetPoint.size() - 1; j >= 0; --j)//画出目标点对应的矩形
 		{
 			minLoc = TargetPoint[j];
 			rectangle(srcResult, minLoc, Point(minLoc.x + templat.cols, minLoc.y + templat.rows), cvScalar(0, 0, 255));
@@ -155,10 +156,10 @@ int main()
 		imwrite("a.png", srcResult);
 		cvWaitKey(0);
 		//imshow("template", templat);
-		
+
 	}
 
-	
+
 
 	return 0;
 }
