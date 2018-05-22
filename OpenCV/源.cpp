78fa4b648c230ef17c772f980d954a6aa16e50c0
昕宇 王 <2250306810@qq.com>
2018-05-22 15:31:19 +0800

    最后的检验改为全图所有点都参与，效果不错，勉强和OpenCV内置函数媲美

--- OpenCV/源.cpp
+++ OpenCV/源.cpp
@@ -9,101 +9,53 @@
 using namespace std;
 using namespace cv;
 
-
-Point getNextMinLoc(Mat result, Point minLoc, int maxVaule, int templatW, int templatH)
-{
-
-	// 先将第一个最小值点附近两倍模板宽度和高度的都设置为最大值防止产生干扰  
-	int startX = minLoc.x - templatW;
-	int startY = minLoc.y - templatH;
-	int endX = minLoc.x + templatW;
-	int endY = minLoc.y + templatH;
-	if (startX < 0) startX = 0;
-
-	if (startY < 0) startY = 0;
-
-	if (endX > result.cols - 1) endX = result.cols - 1;
-
-	if (endY > result.rows - 1)	endY = result.rows - 1;
-
-	int y, x;
-	float *p;
-	for (y = startY; y <= endY; y++)
-	{
-		p = result.ptr<float>(y);
-		for (x = startX; x <= endX; x++)
-		{
-			p[x] = maxVaule;
-		}
-	}
-	// 然后得到下一个最小值并且返回  
-	double new_minVaule, new_maxValue;
-	Point new_minLoc, new_maxLoc;
-	minMaxLoc(result, &new_minVaule, &new_maxValue, &new_minLoc, &new_maxLoc, Mat());
-	return new_minLoc;
-}
-
-bool AddPoint(vector<Point2i> &TargetPoint, Point newPoint, int templatW, int templatH)
-{
-	for (int j = TargetPoint.size() - 1; j >= 0; --j)//去掉相邻点（目标很靠近边界时,完整和不完整匹配都会响应）
-	{
-		if (abs(TargetPoint[j].x - newPoint.x) < templatW && abs(TargetPoint[j].y - newPoint.y) < templatH) //与的关系
-			return 0;
-	}
-
-	TargetPoint.push_back(newPoint);
-	return 1;
-}
+bool wxyMatchTemplate(Mat src, Mat templat, Mat result);
+Point getNextMinLoc(Mat result, Point minLoc, int maxVaule, int templatW, int templatH);
+bool AddPoint(vector<Point2i> &TargetPoint, Point newPoint, int templatW, int templatH);
+void wxyIntegral(Mat source, Mat integral);
+void pickPoints(Mat tempIntegral, vector<Point2i> &calculatePoint, Point center);
 
 
-void wxyIntegral(Mat source, Mat integral) //source和integral都是int型
-{
-	int sourceW = source.cols;
-	int sourceH = source.rows;
-
-	int *p = integral.ptr<int>(0);
-	for (int a = 0; a <= sourceW; a++)  //第一行全为0
-		p[a] = 0;
-
-	for (int y = 0; y < sourceH; y++) {
-		uchar *lineSrc = source.ptr<uchar>(y);
-		int *intePre = integral.ptr<int>(y);
-		int *inteNow = integral.ptr<int>(y+1);
-		inteNow[0] = 0;
-		int sum = 0;
-		for (int x = 0; x < sourceW; x++) {			
-			sum += (lineSrc[x] == 0) ? 1 : 0;
-			inteNow[x + 1] = intePre[x + 1] + sum;
-		}
-	}
-}
-
-void pickPoints(Mat tempIntegral, vector<Point2i> &calculatePoint, Point center)//保证取得点，模板的积分图对应点不为零（除数）
+float accum(Mat srcIntegral, Mat tempIntegral, Point calPoint) //calPoint为要计算的点,以积分图为基准坐标（多一行列）
 {
 	int tempW = tempIntegral.cols-1;
 	int tempH = tempIntegral.rows-1;
-	int pointNum = 2*log2(tempW*tempH);
 
-	srand(GetTickCount());
-	int halftempW = tempW / 2;
-	int halftempH = tempH / 2;
-	int product = center.x*center.y;
+	int *tempLine, *srcLine;
+	float *ratioLine;
+	int *firstLineSrc = srcIntegral.ptr<int>(calPoint.y - 1);
+	int pointInte = 0;
+	int orignInte = srcIntegral.at<int>(calPoint.y-1, calPoint.x-1);
+	float sum = 0;
+	float accum = 0;
 
-	cout << center << endl;
+	Mat ratio(tempH, tempW, CV_32FC1);
 
-	int x, y;
+	for (int y = 0; y < tempH; y++) {//点calPoint是包含在待计算图像中的左上角点
+		tempLine = tempIntegral.ptr<int>(y + 1);
+		srcLine = srcIntegral.ptr<int>(calPoint.y + y);
+		ratioLine = ratio.ptr<float>(y);
+		for (int x = 0; x < tempW; x++) {
+			pointInte = srcLine[calPoint.x + x] + orignInte - firstLineSrc[calPoint.x + x] - srcLine[calPoint.x - 1];
+			//if (tempLine[x + 1] == 0) continue;
 
-	for (int i = 0; i < pointNum; i++) {
-		do {
-			x = rand() % tempW;
-			y = rand() % tempH;
-
-		} while (tempIntegral.at<int>(y, x) == 0 || x*y < product);
-
-		calculatePoint.push_back(Point2i(x, y));
+			ratioLine[x] = (float)(pointInte + 1) / (float)(tempLine[x + 1] + 1);
+			sum += ratioLine[x];
+		}
 	}
 
+	float mean = sum / (tempH*tempW);
+
+	for (int m = 0; m < tempH; m++) {
+		ratioLine = ratio.ptr<float>(m);
+		for (int n = 0; n < tempW; n++) {
+			accum += (ratioLine[n] - mean)*(ratioLine[n] - mean);
+		}
+	}
+
+	return sqrt(accum / (tempH*tempW - 1));
 }
+
 
 
 bool wxyMatchTemplate(Mat src, Mat templat, Mat result)
@@ -163,14 +115,14 @@ bool wxyMatchTemplate(Mat src, Mat templat, Mat result)
 			mean = 0;
 			pass = 0;
 			for (int i = 0; i < pointsNum; i++) {
-				pointIntegral = srcIntegral.at<int>(y + calculatePoint[i].y, x + calculatePoint[i].x) + srcIntegral.at<int>(y, x) - srcIntegral.at<int>(y + calculatePoint[i].y, x) - srcIntegral.at<int>(y, x + calculatePoint[i].x);
-				ratio[i] = pointIntegral / tempPoint[i];
+				pointIntegral = srcIntegral.at<int>(y + calculatePoint[i].y, x + calculatePoint[i].x) + srcIntegral.at<int>(y - 1, x - 1) - srcIntegral.at<int>(y + calculatePoint[i].y, x - 1) - srcIntegral.at<int>(y - 1, x + calculatePoint[i].x);
+				ratio[i] = pointIntegral / tempPoint[i];//上式点（x,y）是包含在待计算图像中的左上角点
 				if (ratio[i] > 1.2 || ratio[i] < 0.8) {
 					pass = 1;
 					break;
 				}
 				sum += ratio[i];
-				mean = sum / (i + 1);
+				
 				/*if (mean > 1.05 || mean < 0.95){ 
 					pass = 1; 
 					break; 
@@ -179,12 +131,14 @@ bool wxyMatchTemplate(Mat src, Mat templat, Mat result)
 
 			if (pass)   continue;
 			findFlag = 1;
+			p[x - 1] = accum(srcIntegral, tempIntegral, Point(x, y));
 
+			/*mean = sum / pointsNum;
 			float accum = 0.0;
 			for (int j = 0; j < pointsNum; j++) {
 				accum += (ratio[j] - mean)*(ratio[j] - mean);
 			}
-			p[x-1] = sqrt(accum / (pointsNum - 1));//方差
+			p[x-1] = sqrt(accum / (pointsNum - 1));//方差*/
 		}
 	}
 	if (findFlag)
@@ -302,4 +256,99 @@ int main()
 	}
 
 	return 0;
+}
+
+Point getNextMinLoc(Mat result, Point minLoc, int maxVaule, int templatW, int templatH)
+{
+
+	// 先将第一个最小值点附近两倍模板宽度和高度的都设置为最大值防止产生干扰  
+	int startX = minLoc.x - templatW;
+	int startY = minLoc.y - templatH;
+	int endX = minLoc.x + templatW;
+	int endY = minLoc.y + templatH;
+	if (startX < 0) startX = 0;
+
+	if (startY < 0) startY = 0;
+
+	if (endX > result.cols - 1) endX = result.cols - 1;
+
+	if (endY > result.rows - 1)	endY = result.rows - 1;
+
+	int y, x;
+	float *p;
+	for (y = startY; y <= endY; y++)
+	{
+		p = result.ptr<float>(y);
+		for (x = startX; x <= endX; x++)
+		{
+			p[x] = maxVaule;
+		}
+	}
+	// 然后得到下一个最小值并且返回  
+	double new_minVaule, new_maxValue;
+	Point new_minLoc, new_maxLoc;
+	minMaxLoc(result, &new_minVaule, &new_maxValue, &new_minLoc, &new_maxLoc, Mat());
+	return new_minLoc;
+}
+
+bool AddPoint(vector<Point2i> &TargetPoint, Point newPoint, int templatW, int templatH)
+{
+	for (int j = TargetPoint.size() - 1; j >= 0; --j)//去掉相邻点（目标很靠近边界时,完整和不完整匹配都会响应）
+	{
+		if (abs(TargetPoint[j].x - newPoint.x) < templatW && abs(TargetPoint[j].y - newPoint.y) < templatH) //与的关系
+			return 0;
+	}
+
+	TargetPoint.push_back(newPoint);
+	return 1;
+}
+
+
+void wxyIntegral(Mat source, Mat integral) //source和integral都是int型
+{
+	int sourceW = source.cols;
+	int sourceH = source.rows;
+
+	int *p = integral.ptr<int>(0);
+	for (int a = 0; a <= sourceW; a++)  //第一行全为0
+		p[a] = 0;
+
+	for (int y = 0; y < sourceH; y++) {
+		uchar *lineSrc = source.ptr<uchar>(y);
+		int *intePre = integral.ptr<int>(y);
+		int *inteNow = integral.ptr<int>(y+1);
+		inteNow[0] = 0;
+		int sum = 0;
+		for (int x = 0; x < sourceW; x++) {			
+			sum += (lineSrc[x] == 0) ? 1 : 0;
+			inteNow[x + 1] = intePre[x + 1] + sum;
+		}
+	}
+}
+
+void pickPoints(Mat tempIntegral, vector<Point2i> &calculatePoint, Point center)//保证取得点，模板的积分图对应点不为零（除数）
+{
+	int tempW = tempIntegral.cols-1;
+	int tempH = tempIntegral.rows-1;
+	int pointNum = 2*log2(tempW*tempH);
+
+	srand(GetTickCount());
+	int halftempW = tempW / 2;
+	int halftempH = tempH / 2;
+	int product = center.x*center.y;
+
+	cout << center << endl;
+
+	int x, y;
+
+	for (int i = 0; i < pointNum; i++) {
+		do {
+			x = rand() % tempW;
+			y = rand() % tempH;
+
+		} while (tempIntegral.at<int>(y, x) == 0 || x*y < product);
+
+		calculatePoint.push_back(Point2i(x, y));
+	}
+
 }
